7f2d40313fcb5fae7751046c64f38ea243fd7a00
Aser13557 <4228850+Aser13557@users.noreply.github.com>
2026-08-28 20:29:04 +0300

    Add files via upload
    [skip ci]

--- scenarios/summarize.sh
+++ scenarios/summarize.sh
@@ -2,4 +2,5 @@
 source "$(dirname "${BASH_SOURCE[0]}")/env.sh"
 c_head "summarize"
 node "${ROOT}/tools/build_replay.js"
+node "${ROOT}/tools/render_check.js" --print | tee "${RESULTS}/render_check.txt"
 node "${ROOT}/tools/feasibility_summary.js"
